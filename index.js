68631a083a560b08ffd3edf934c919feb4225713
feltom1000 <feltom1000@hotmail.com>
2021-05-21 22:45:45 -0300

    Todo roto, solo funciona la lista de objetos inicial

--- index.js
+++ index.js
@@ -1,19 +1,55 @@
 let vueltas = [];
+let duraciones = [];
 
-function NuevaVuelta(nroVueltaValor, durationValor){
-    vueltas.push({nroVuelta: nroVueltaValor, Duracion: durationValor})
+function NuevaVuelta(nroVueltaValor, durationValor, circuitoKm){
+    vueltas.push({nroVuelta: nroVueltaValor, duracion: durationValor, circuito: circuitoKm + "Km"})
 }
+
+function llenarArrayDeDuraciones(){
+    vueltas.forEach(function(vuelta){
+        duraciones.push(vuelta.duracion)
+    })
+}
+
+function convertirAPuntosFlotantes(){
+    for(i = 0; i < duraciones.length; i++){
+        parseFloat(duraciones[i])
+    }
+}
+
+function velocidadMedia(velocidadMax, velocidadMin, distancia){
+    return distancia / (velocidadMax - velocidadMin)
+}
+
+
+let extensionKm = prompt("Ingresa la extension en Km tiene del circuito recorrido")
+let extensionKmN = Number(extensionKm)
 
 let cantidad = prompt("Ingresa la cantidad de vueltas que deseas registrar")
 let cantidadN = Number(cantidad)
+
 
 while(cantidadN > 0){
 let nroVuelta = prompt("Ingresa el Nro de la vuelta a ingresar")
 let nroVueltaN = Number(nroVuelta)
 
-let duration = prompt("Ingresa la duración de la vuelta")
-let durationN = Number(duration)
-NuevaVuelta(nroVuelta, duration)
+let duration = prompt("Ingresa el tiempo de la vuelta en formato MM,SS")
 
+
+NuevaVuelta(nroVueltaN, duration, extensionKmN)
 cantidadN -= 1
 }
+
+llenarArrayDeDuraciones();
+convertirAPuntosFlotantes();
+let vueltaMasRapida = Math.min(duraciones);
+console.log(vueltaMasRapida);
+
+let vueltaMasLenta = Math.max(duraciones);
+console.log(vueltaMasLenta);
+
+velocidadMedia(vueltaMasRapida, vueltaMasLenta, extensionKmN);
+
+
+
+
