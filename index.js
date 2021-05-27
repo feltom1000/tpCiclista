ce31924f0d7caf88accec77397396b6015a253fc
feltom1000 <feltom1000@hotmail.com>
2021-05-26 21:19:55 -0300

    Las duraciones de vueltas estan separadas en arrays(int) de min y de seg. Aun sin solución para los calculos de vueltas mas rapidas o lentas

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 let vueltas = [];
 let duraciones = [];
+let spliter = [];
+let duracionesSpliteadas = [];
+let numerosSpliteados = [];
+let minutos = [];
+let segundos = [];
+let indexDeMinutosIguales = []
+let vueltaRapida
+let vueltaLenta
 
 function NuevaVuelta(nroVueltaValor, durationValor, circuitoKm){
     vueltas.push({nroVuelta: nroVueltaValor, duracion: durationValor, circuito: circuitoKm + "Km"})
-}
-
-function llenarArrayDeDuraciones(){
-    vueltas.forEach(function(vuelta){
-        duraciones.push(vuelta.duracion)
-    })
-}
-
-function convertirAPuntosFlotantes(){
-    for(i = 0; i < duraciones.length; i++){
-        parseFloat(duraciones[i])
-    }
 }
 
 function velocidadMedia(velocidadMax, velocidadMin, distancia){
@@ -30,25 +26,84 @@ let cantidadN = Number(cantidad)
 
 
 while(cantidadN > 0){
-let nroVuelta = prompt("Ingresa el Nro de la vuelta a ingresar")
-let nroVueltaN = Number(nroVuelta)
-
-let duration = prompt("Ingresa el tiempo de la vuelta en formato MM,SS")
-
-
-NuevaVuelta(nroVueltaN, duration, extensionKmN)
-cantidadN -= 1
+    let nroVuelta = prompt("Ingresa el Nro de la vuelta a ingresar")
+    let nroVueltaN = Number(nroVuelta)
+    let duration = prompt("Ingresa el tiempo de la vuelta")
+    NuevaVuelta(nroVueltaN, duration, extensionKmN)
+    cantidadN -= 1
 }
 
-llenarArrayDeDuraciones();
-convertirAPuntosFlotantes();
-let vueltaMasRapida = Math.min(duraciones);
-console.log(vueltaMasRapida);
+duraciones = vueltas.map(function(vuelta){
+    return vuelta.duracion
+})
 
-let vueltaMasLenta = Math.max(duraciones);
-console.log(vueltaMasLenta);
+function spliteador(){
+    for(let i = 0; i < duraciones.length; i++){
+        spliter = duraciones[i].split(":")
+        for(let j = 0; j < spliter.length; j++){
+            duracionesSpliteadas.push(spliter[j])
+        }
+    }
+}
 
-velocidadMedia(vueltaMasRapida, vueltaMasLenta, extensionKmN);
+function integerSplit(){
+    for (let i = 0; i < duracionesSpliteadas.length; i++){
+        numerosSpliteados.push(parseInt(duracionesSpliteadas[i]))
+    }
+}
+
+function minutosDeVueltas(){
+    for(let i = 0; i < numerosSpliteados.length; i += 2){
+        minutos.push(numerosSpliteados[i])
+    }
+}
+
+function segundosDeVueltas(){
+    for(let i = 1; i < numerosSpliteados.length; i += 2){
+        segundos.push(numerosSpliteados[i])
+    }
+}
+
+function vueltaMasRapida(){
+    if (comprobadorDeTiemposIguales() == true){
+        Math.min(segundos[indexDeMinutosIguales])
+    }
+}
+
+
+
+function comprobadorDeTiemposIguales(){
+    for (let i = 0; i < minutos.length; i++) {
+        for (let j = 1; j < minutos.length; j++) {
+            if (minutos[i] === minutos[j]) {
+              if (segundos[i] === segundos[j]){
+                  console.log(vueltas[i])
+                  console.log(vueltas[j])
+              } else {
+                indexDeMinutosIguales.push(i)
+                indexDeMinutosIguales.push(j)
+                return true
+              }
+            } else {
+                return false
+            }
+        }
+    }
+}
+
+spliteador()
+integerSplit()
+minutosDeVueltas()
+segundosDeVueltas()
+
+
+// let vueltaMasRapida = Math.min((duraciones);
+// console.log(vueltaMasRapida);
+
+// let vueltaMasLenta = Math.max(duraciones);
+// console.log(vueltaMasLenta);
+
+// velocidadMedia(vueltaMasRapida, vueltaMasLenta, extensionKmN);
 
 
 
