85f24a03046630e302fc9ca68f2d0f05e4e9064e
feltom1000 <feltom1000@hotmail.com>
2021-05-27 23:11:49 -0300

    Casi finalizada la etapa 2. Funcion de velocidadDeLasVueltas rota.

--- index.js
+++ index.js
@@ -4,19 +4,23 @@ let spliter = [];
 let duracionesSpliteadas = [];
 let numerosSpliteados = [];
 let duracionEnSegundos = [];
+let velocidadDeLAsVueltas = [];
+let vueltaIngresadaParaVerInfo = [];
 let vueltaMasRapidaEnSegundos
 let indexVueltaMasRapida
 let vueltaMasLentaEnSegundos
 let indexVueltaMasLenta
 
-function NuevaVuelta(nroVueltaValor, durationValor, circuitoKm){
-    vueltas.push({nroVuelta: nroVueltaValor, duracion: durationValor, circuito: circuitoKm + "Km"})
+
+function NuevaVuelta(nroVueltaValor, durationValor, circuitoKm, fecha){
+    vueltas.push({nroVuelta: nroVueltaValor, duracion: durationValor, circuito: circuitoKm + "Km", fecha: fecha})
 }
 
 function velocidadMedia(velocidadMax, velocidadMin, distancia){
     return distancia / (velocidadMax - velocidadMin)
 }
 
+let fechaDeEntrenamiento = prompt("Ingrese la fecha de su entrenamiento")
 
 let extensionKm = prompt("Ingresa la extension en Km tiene del circuito recorrido")
 let extensionKmN = Number(extensionKm)
@@ -29,9 +33,19 @@ while(cantidadDeVueltasN > 0){
     let nroVuelta = prompt("Ingresa el Nro de la vuelta a ingresar")
     let nroVueltaN = Number(nroVuelta)
     let duration = prompt("Ingresa el tiempo de la vuelta")
-    NuevaVuelta(nroVueltaN, duration, extensionKmN)
+    NuevaVuelta(nroVueltaN, duration, extensionKmN, fechaDeEntrenamiento)
     cantidadDeVueltasN -= 1
 }
+
+vueltas.sort(function(a,b){
+    if(a.nroVuelta < b.nroVuelta){
+        return -1
+    }
+    if(a.nroVuelta > b.nroVuelta){
+        return 1
+    }
+    return 0
+})
 
 duraciones = vueltas.map(function(vuelta){
     return vuelta.duracion
@@ -78,16 +92,39 @@ function vueltaMasLenta(){
 }
 
 function velocidadMedia(){
-    let velocidadMediaEnMxS = ((extensionKmN * 1000) / (vueltaMasLentaEnSegundos - vueltaMasRapidaEnSegundos))
+    let velocidadMediaEnMxS = ((extensionKmN * 1000) / (duracionEnSegundos[0] - duracionEnSegundos[duracionEnSegundos.length - 1]))
     console.log("Velocidad media: " + velocidadMediaEnMxS + "m/s")
 }
+
+for(let i = 0; i < duracionEnSegundos.length; i++){
+    velocidadDeLAsVueltas.push((extensionKmN * 1000) / duracionEnSegundos[i])
+} // Esto no funciona aun.
+
+let vueltasPorArribaDeLaMedia = velocidadDeLAsVueltas.filter(function(velocidades){
+    return velocidades > velocidadMediaEnMxS
+})
+
+function ingresarNumeroDeUnaVuelta(){
+    let infoDeVueltas = prompt("Ingrese el numero de la vuelta cuya información desea ver")
+    vueltaIngresadaParaVerInfo = vueltas.find(function(vuelta){
+        return vuelta.nroVuelta == infoDeVueltas
+    })
+}
+
 
 spliteador()
 integerSplit()
 convertirLasDuracionesASegundos()
+console.log(vueltas)
 vueltaMasRapida()
 vueltaMasLenta()
 velocidadMedia()
+console.log(vueltasPorArribaDeLaMedia)
+ingresarNumeroDeUnaVuelta()
+console.log(vueltaIngresadaParaVerInfo)
+
+
+
 
 
 
